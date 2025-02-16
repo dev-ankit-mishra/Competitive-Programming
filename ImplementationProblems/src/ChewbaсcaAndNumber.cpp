29852cefab3fd5fad14733938d0ad3066c85211d
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-02-16 23:08:42 +0530

    Chewbaсca and Number

---
+++ ImplementationProblems/src/ChewbaсcaAndNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    int i = 0;
+    while (i < str.size())
+    {
+        char ch = str[i];
+        int t = ch - '0';
+        int invert = 9 - t;
+        int ans = min(t, invert);
+        str[i] = ans + '0';
+        i++;
+    }
+
+    if (str[0] == '0')
+        str[0] = '9';
+    cout << str << endl;
+
+    return 0;
+}
